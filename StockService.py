a87abe52e3fb4b4f71461ad390f6607955143080
Gerard Lucas <gerard.lucas@conperio.com>
2023-10-24 10:10:21 -0500

    service refactor

--- StockService.py
+++ StockService.py
@@ -4,20 +4,20 @@ from Models import Stock, TimeSeries
 from StockExceptions import StockQueryException
 
 class StockService:
+    BASE_URL = "https://www.alphavantage.co/query"
+
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def __query_api(self, data_type: str, symbol: str):
-        url = "https://www.alphavantage.co/query?function={}&symbol={}&apikey={}".format(data_type, symbol, self.api_key)
-        
-        # time series intraday requires an interval parameter
-        if data_type == "TIME_SERIES_INTRADAY":
-            url += "&interval=5min"
-        # time series daily requires an outputsize parameter
-        elif data_type == "TIME_SERIES_DAILY":
-            url += "&outputsize=full"
-        
-        response = requests.get(url)
+    def __query_api(self, data_type: str, symbol: str, additional_params: dict = {}):
+        params = {
+            "function": data_type,
+            "symbol": symbol,
+            "apikey": self.api_key
+        }
+        params.update(additional_params)
+
+        response = requests.get(self.BASE_URL, params=params)
         # ensure the request was successful
         if response.status_code != 200:
             raise StockQueryException("API returned an http error. Add detail to this message?")
@@ -49,21 +49,17 @@ class StockService:
             raise StockQueryException("API returned an unexpected response.")
 
     def get_intraday(self, symbol: str) -> TimeSeries:
-        res = self.__query_api("TIME_SERIES_INTRADAY", symbol)
-        time_series = self.__create_time_series(symbol, res, 'Time Series (5min)')
-        return time_series
+        res = self.__query_api("TIME_SERIES_INTRADAY", symbol, {"interval": "5min"})
+        return self.__create_time_series(symbol, res, 'Time Series (5min)')
         
     def get_daily(self, symbol: str) -> TimeSeries:
-        res = self.__query_api("TIME_SERIES_DAILY", symbol)
-        time_series = self.__create_time_series(symbol, res, 'Time Series (Daily)')
-        return time_series
+        res = self.__query_api("TIME_SERIES_DAILY", symbol, {"outputsize": "full"})
+        return self.__create_time_series(symbol, res, 'Time Series (Daily)')
     
     def get_weekly(self, symbol: str) -> TimeSeries:
         res = self.__query_api("TIME_SERIES_WEEKLY", symbol)
-        time_series = self.__create_time_series(symbol, res, 'Weekly Time Series')
-        return time_series
+        return self.__create_time_series(symbol, res, 'Weekly Time Series')
     
     def get_monthly(self, symbol: str) -> TimeSeries:
         res = self.__query_api("TIME_SERIES_MONTHLY", symbol)
-        time_series = self.__create_time_series(symbol, res, 'Monthly Time Series')
-        return time_series
+        return self.__create_time_series(symbol, res, 'Monthly Time Series')
