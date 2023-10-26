54cf0bd61d650c31dd7aa89f2cf4dd409413000b
jacobtimlin <94022955+jacobtimlin@users.noreply.github.com>
2023-10-26 00:11:08 -0500

    Finished up a general idea for graphing

--- StockChart.py
+++ StockChart.py
@@ -1,34 +1,47 @@
 import pygal
 from datetime import datetime, timedelta
 import lxml
-import Utility as util
+from Utility import Utility
 from Models import Stock, TimeSeries
 
 class StockChart:
     def graphData(self, chartType, time_series: TimeSeries):
         #if 1 then bar graph
-        if(chartType == 1):
-            bar_chart = pygal.Bar()
-            bar_chart.title = f'Stock Data for {time_series.symbol}: {time_series.start_date} to {time_series.end_date}'
-            date_range = [time_series.start_date + timedelta(days=i) for i in range((time_series.end_date - time_series.start_date).days + 1)]
-            date_strings = [date.strftime('%Y-%m-%d') for date in date_range]
-
+        if(chartType == 2):
+            line_chart = pygal.Line(x_label_rotation=35)
+            line_chart.title = f'Stock Data for {time_series.symbol}: {time_series.start_date} to {time_series.end_date}'
+            time_series.series.reverse()
+            line_chart.x_labels = [stock.date for stock in time_series.series]
+            
             # use time_series.sersies for pygal chart
-            #bar_chart.x_labels = map(str, range(time_series.start_date, time_series.end_date))
-            bar_chart.x_labels = date_strings
-            open = [item['open'] for item in time_series.series]
-            high = [item['high'] for item in time_series.series]
-            low = [item['low'] for item in time_series.series]
-            close = [item['close'] for item in time_series.series]
+            open = [item.open for item in time_series.series]
+            high = [item.high for item in time_series.series]
+            low = [item.low for item in time_series.series]
+            close = [item.close for item in time_series.series]
+            line_chart.add('Open', open)
+            line_chart.add('High', high)
+            line_chart.add('Low', low)
+            line_chart.add('Close', close)
+            
+            # show in browser with lxml
+            line_chart.render_in_browser()
+        if(chartType == 1):
+            bar_chart = pygal.Bar(x_label_rotation=35)
+            bar_chart.title = f'Stock Data for {time_series.symbol}: {time_series.start_date} to {time_series.end_date}'
+            time_series.series.reverse()
+            bar_chart.x_labels = [stock.date for stock in time_series.series]
+            
+            # use time_series.sersies for pygal chart
+            open = [item.open for item in time_series.series]
+            high = [item.high for item in time_series.series]
+            low = [item.low for item in time_series.series]
+            close = [item.close for item in time_series.series]
             bar_chart.add('Open', open)
             bar_chart.add('High', high)
             bar_chart.add('Low', low)
             bar_chart.add('Close', close)
-            # for x in time_series.series:
-                
-            #     bar_chart.add(x.date, [x.open, x.high, x.low, x.close])
-            # bar_chart.x_labels = map(str, range(time_series.start_date, time_series.end_date))
             
+            # show in browser with lxml
             bar_chart.render_in_browser()
 
 
--- StockDriver.py
+++ StockDriver.py
@@ -99,15 +99,8 @@ def getStockData(service: StockService, ticker: str, time_series: int, start_dat
 def main():
     load_dotenv()
     serv = StockService(os.getenv("API_KEY"))
-
-    # Temp: Uncomment to test service without user input
-    #temp_data = getStockData(serv, "AAPL", 2, datetime.datetime(2020, 1, 1), datetime.datetime(2020, 12, 31))
-    #temp_intra = getStockData(serv, "AAPL", 1, datetime.datetime(2020, 1, 1), datetime.datetime(2020, 9, 28))
-    temp_week = getStockData(serv, "AAPL", 3, datetime.datetime(2020, 1, 1), datetime.datetime(2020, 12, 31))
     chart_serv = StockChart()
-    chart_serv.graphData(1, temp_week)
-    #temp_month = getStockData(serv, "AAPL", 4, datetime.datetime(2020, 1, 1), datetime.datetime(2020, 12, 31))
-
+    
     while True:
         ticker = getTik()
         chartType = getChartType()
@@ -119,15 +112,17 @@ def main():
             validDates = checkDates(startDate, endDate)
 
         # get stock data from api
+        print(f"Getting data for {ticker} from {startDate} to {endDate} of series {timeSeries}...")
         stockData = getStockData(serv, ticker, timeSeries, startDate, endDate)
         # check if stock data was returned, otherwise an error occurred,
         # was printed, and we should continue to the next iteration or exit
-        if stockData == None:
+        print(stockData)
+        if stockData == None or stockData.series == None or len(stockData.series) == 0:
             continue
+        print('passed')
+        # graph the data
+        chart_serv.graphData(chartType, stockData)
         
-        
-
-
         if goAgain() == False:
             break
 
